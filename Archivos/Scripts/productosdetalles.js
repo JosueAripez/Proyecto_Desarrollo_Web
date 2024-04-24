5fab20632d4fe84434c04d3ee8baf87f8080963d
Marcelo Marquez <marcelo.marquez@uabc.edu.mx>
2024-04-23 21:33:58 -0700

    cambio en html productos

--- Archivos/Scripts/productosdetalles.js
+++ Archivos/Scripts/productosdetalles.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtener referencias a elementos HTML relevantes
     const slider = document.querySelector('.slider');
     const slides = document.querySelectorAll('.slide');
     const totalSlides = slides.length;
 
     let currentSlide = 0;
 
-    // Función para mostrar el slide actual
     function showSlide(index) {
         slides.forEach((slide, i) => {
             if (i === index) {
@@ -17,22 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Función para avanzar al siguiente slide
     function nextSlide() {
         currentSlide = (currentSlide + 1) % totalSlides;
         showSlide(currentSlide);
     }
 
-    // Función para retroceder al slide anterior
     function prevSlide() {
         currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
         showSlide(currentSlide);
     }
 
-    // Mostrar el primer slide al cargar la página
     showSlide(currentSlide);
 
-    // Agregar eventos para los controles de navegación
     document.querySelector('.next').addEventListener('click', nextSlide);
     document.querySelector('.prev').addEventListener('click', prevSlide);
 });
