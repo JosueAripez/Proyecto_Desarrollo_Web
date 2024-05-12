d9395e3eb13f019581dd3d6303d1b7c5d79289c0
Marcelo Marquez Ontiveros <marcelo.marquez@uabc.edu.mx>
2024-05-11 17:46:29 -0700

    sdgs

--- Archivos/Scripts/productosdetalles.js
+++ Archivos/Scripts/productosdetalles.js
@@ -34,3 +34,16 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.prev').addEventListener('click', prevSlide);
 });
 
+window.addEventListener('load',function(){
+    new Glider(document.querySelector('.carousel__lista'),{
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: '.carousel__indicadores',
+        draggable: true,
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+        }
+    });
+    
+});
