25bcca4efa69b7cf1f828980e22b80b1857a7860
Marcelo Marquez Ontiveros <marcelo.marquez@uabc.edu.mx>
2024-05-12 12:33:47 -0700

    asfaf

--- Archivos/Scripts/productosdetalles.js
+++ Archivos/Scripts/productosdetalles.js
@@ -1,45 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector('.slider');
-    const slides = document.querySelectorAll('.slide');
-    const totalSlides = slides.length;
 
-    let currentSlide = 0;
-
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            if (i === index) {
-                slide.style.display = 'block';
-                slide.classList.add('active'); // Agrega la clase 'active' a la diapositiva actual
-
-            } else {
-                slide.style.display = 'none';
-                slide.classList.remove('active'); // Elimina la clase 'active' de las diapositivas que no son la actual
-            }
-        });
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % totalSlides;
-        showSlide(currentSlide);
-    }
-
-    function prevSlide() {
-        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-        showSlide(currentSlide);
-    }
-
-    showSlide(currentSlide);
-
-    document.querySelector('.next').addEventListener('click', nextSlide);
-    document.querySelector('.prev').addEventListener('click', prevSlide);
-});
 
 window.addEventListener('load',function(){
     new Glider(document.querySelector('.carousel__lista'),{
         slidesToShow: 1,
         slidesToScroll: 1,
+        scrollLock: true,
         dots: '.carousel__indicadores',
-        draggable: true,
+
         arrows: {
             prev: '.carousel__anterior',
             next: '.carousel__siguiente'
